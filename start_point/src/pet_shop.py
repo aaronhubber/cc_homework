12a4ec77600aee9e18596d4f9e97d4d945df1829
Aaron Hubber <aaronhubber@gmail.com>
2022-04-02 11:06:24 +0100

    up to q14

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -1,6 +1,10 @@
 # WRITE YOUR FUNCTIONS HERE
 #to get the name of the petshop, return the name
 #1
+from os import remove
+from time import perf_counter
+
+
 def get_pet_shop_name (shop):
     return shop["name"]
 
@@ -29,9 +33,70 @@ def get_stock_count(pets):
     return len(pets["pets"])
 
 #8 find the number of britih shorthairs
-def get_pets_by_breed (number_of_breed, breed):
-    
-    for breed in 
+def get_pets_by_breed (pet_shop, breed):
+    num_breed = []
+    pets = pet_shop["pets"]
+    for pet in pets:
+        if pet["breed"] == breed:
+            num_breed.append(pet)
+    return (num_breed)
+
+#9 same as number 8
+
+#10 same as above but looking for name rather than breed
+
+def find_pet_by_name(pet_shop, pet_name):
+    for pets_select in pet_shop["pets"]:
+        if pet_name == pets_select["name"]:
+            return pets_select
+
+# def find_pet_by_name (pet_shop, name):
+#     pets = pet_shop["pets"]
+#     for pet in pets:
+#         if pet["name"] == name:
+#             return pet["name"]
+
+
+# def find_pet_by_name (pet_shop, names):
+#     pet_names = pet_shop["pets"]
+#     for banana in pet_shop:
+#         if banana["name"] == names:
+#             return (pet_names)
+
+    # pet = []
+    # names = pet_shop["name"]
+    # for petz in names:
+    #     if petz["pet"]["name"] == [p_names]:
+    #         pet.append(p_names)
+    # return pet
+
+# def find_pet_by_name (pet_shop, pet_names):
+#     pet_names = []
+#     for names in pet_names:
+#         for name in names ["pets"]["names"]:
+#             pet_names.append(name)
+#     return pet_names
+
+#q11 same as 10
+
+# q12 remove arthur from list
+
+def remove_pet_by_name (pet_shop, pet_name):
+    for pets_select in pet_shop["pets"]:
+        if pet_name == pets_select["name"]:
+            return pet_name
+
+#q13 
+
+def remove_pet_by_name(pet_shop, pet_name):
+    #pets = pet_shop["pets"]
+
+    for pet in pet_shop["pets"] :
+        if pet["name"] == pet_name:
+            pet_shop["pets"].remove(pet)
+
+#q14
+ 
 
 
 
